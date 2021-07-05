9ce7e5e98dd4d51e31fb197de6a9cda168a51555
jslopes <jslopes@uoldiveo.com>
2021-07-05 16:18:22 -0300

    add listerner https

---
+++ README.md
@@ -0,0 +1,15 @@
+# Terraform Module Application Load Balance
+
+## Usage
+
+## Requirements
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Variables Inputs
+| Name | Description | Required | Type | Default |
+| ---- | ----------- | -------- | ---- | ------- |
+
+## Variable Outputs
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+| Name | Description |
+| ---- | ----------- |
--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 
 # ELBv2 - Application Load Balance
 resource "aws_lb" "lb" {
-    name                  = var.name
-    internal              = var.internal
-    load_balancer_type    = var.load_balancer_type
-    security_groups       = var.security_groups
-    subnets               = var.subnets
+  name                = var.name
+  internal            = var.internal
+  load_balancer_type  = var.load_balancer_type
+  security_groups     = var.security_groups
+  subnets             = var.subnets
 
-    enable_deletion_protection = var.enable_deletion_protection
+  enable_deletion_protection = var.enable_deletion_protection
 
-    dynamic "access_logs" {
-        for_each    = length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
-        content {
-            bucket  = lookup(access_logs.value, "bucket", null)
-            prefix  = lookup(access_logs.value, "prefix", null)
-            enabled = lookup(access_logs.value, "enabled", null)
-        }
+  dynamic "access_logs" {
+    for_each  = length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
+    content {
+      bucket  = lookup(access_logs.value, "bucket", null)
+      prefix  = lookup(access_logs.value, "prefix", null)
+      enabled = lookup(access_logs.value, "enabled", null)
     }
+  }
 
-    dynamic "subnet_mapping" {
-        for_each    = var.subnet_mapping
-        content {
-            subnet_id       = lookup(subnet_mapping.value, "subnet_id", null)
-            allocation_id   = lookup(subnet_mapping.value, "allocation_id", null)
-        }
+  dynamic "subnet_mapping" {
+    for_each  = var.subnet_mapping
+    content {
+      subnet_id     = lookup(subnet_mapping.value, "subnet_id", null)
+      allocation_id = lookup(subnet_mapping.value, "allocation_id", null)
     }
+  }
 
-    tags          = var.default_tags
+  tags          = var.default_tags
 }
 
 ############
@@ -34,113 +34,80 @@ resource "aws_lb" "lb" {
 ############
 
 resource "aws_alb_listener" "https_listeners" {
-    count = var.create_lb ? length(var.https_listeners) : 0
+  count = var.create_lb ? length(var.https_listeners) : 0
 
-    load_balancer_arn = aws_lb.lb.arn
+  load_balancer_arn = aws_lb.lb.arn
 
-    port            = var.https_listeners[count.index]["port"]
-    protocol        = lookup(var.https_listeners[count.index], "protocol", "HTTPS")
-    certificate_arn = var.https_listeners[count.index]["certificate_arn"]
-    ssl_policy      = lookup(var.https_listeners[count.index], "ssl_policy", var.listener_ssl_policy_default)
+  port            = var.https_listeners[count.index]["port"]
+  protocol        = lookup(var.https_listeners[count.index], "protocol", "HTTPS")
+  certificate_arn = var.https_listeners[count.index]["certificate_arn"]
+  ssl_policy      = lookup(var.https_listeners[count.index], "ssl_policy", var.listener_ssl_policy_default)
 
-    default_action {
-        target_group_arn = var.https_listeners[count.index]["target_group_arn"]
-        type             = "forward"
-    }
+  default_action {
+    target_group_arn = var.https_listeners[count.index]["target_group_arn"]
+    type             = "forward"
+  }
 }
 
 
 resource "aws_alb_listener" "http_listeners" {
-    count = var.create_lb ? length(var.http_listeners) : 0
+  count = var.create_lb ? length(var.http_listeners) : 0
 
-    load_balancer_arn = aws_lb.lb.arn
+  load_balancer_arn = aws_lb.lb.arn
 
-    port            = var.http_listeners[count.index]["port"]
-    protocol        = lookup(var.http_listeners[count.index], "protocol", "HTTP")
+  port            = var.http_listeners[count.index]["port"]
+  protocol        = lookup(var.http_listeners[count.index], "protocol", "HTTP")
 
-    default_action {
-        target_group_arn = var.http_listeners[count.index]["target_group_arn"]
-        type             = "forward"
-    }
+  default_action {
+    target_group_arn = var.http_listeners[count.index]["target_group_arn"]
+    type             = "forward"
+  }
 }
 
 #################
 # Listener Rule #
 ################
 
-resource "aws_alb_listener_rule" "http_listener_rule" {
-    count = var.create_lb ? length(var.http_listeners) : 0
+resource "aws_lb_listener_rule" "lb_https" {
+  count = var.create_lb ? length(var.https_listeners) : 0
+    
+  listener_arn  = aws_lb_listener.lb_http.0.arn
+  priority      = lookup(var.service_load_balancing_https[count.index], "priority_rule", null)
 
-    listener_arn = aws_alb_listener.http_listeners[0].arn
+  dynamic "action" {
+    for_each = length(keys(lookup(var.service_load_balancing_https[count.index], "redirect_rule", {}))) == 0 ? [] : [lookup(var.service_load_balancing_https[count.index], "redirect_rule", {})]
+    content {
+      type    = lookup(action.value, "type", null)
 
-    priority     = var.http_listeners[count.index]["priority"]
-
-    dynamic "action" {
-        for_each    = var.action
+      dynamic "redirect" {
+        for_each = length(keys(lookup(action.value, "redirect", {}))) == 0 ? [] : [lookup(action.value, "redirect", {})]
         content {
-            target_group_arn    = action.value.target_group_arn
-            type                = action.value.type
-
-            dynamic "redirect" {
-                for_each = length(keys(lookup(action.value, "redirect", {}))) == 0 ? [] : [lookup(action.value, "redirect", {})]
-                content {
-                    port        = redirect.value.port
-                    protocol    = redirect.value.protocol
-                    status_code = redirect.value.status_code
-                }
-            }
+            port        = lookup(redirect.value, "type", "443")
+            protocol    = lookup(redirect.value, "protocol", "HTTPS")
+            status_code = lookup(redirect.value, "status_code", "HTTP_301")
         }
-    }
+      }
 
-    dynamic "condition" {
-        for_each    = var.condition
+    }
+  }
+
+  dynamic "action" {
+    for_each = length(keys(lookup(var.service_load_balancing_https[count.index], "forward_rule", {}))) == 0 ? [] : [lookup(var.service_load_balancing_https[count.index], "forward_rule", {})]
+    content {
+      type              = lookup(action.value, "type", null)
+      target_group_arn  = aws_lb_target_group.lb_https.0.arn
+    }
+  }
+
+  dynamic "condition" {
+    for_each = length(keys(lookup(var.service_load_balancing_https[count.index], "condition", {}))) == 0 ? [] : [lookup(var.service_load_balancing_https[count.index], "condition", {})]
+    content {
+      dynamic "path_pattern" {
+        for_each = length(keys(lookup(condition.value, "path_pattern", {}))) == 0 ? [] : [lookup(condition.value, "path_pattern", {})]
         content {
-            dynamic "path_pattern" {
-                for_each = length(keys(lookup(condition.value, "path_pattern", {}))) == 0 ? [] : [lookup(condition.value, "path_pattern", {})]
-                content {
-                    values  = path_pattern.value.values
-                }
-            }
+          values = lookup(path_pattern.value, "values", null)
         }
+      }
     }
+  }
 }
-
-
-
-resource "aws_alb_listener_rule" "https_listener_rule" {
-    count = var.create_lb ? length(var.https_listeners) : 0
-
-    listener_arn = aws_alb_listener.https_listeners[0].arn
-
-    priority     = var.https_listeners[count.index]["priority"]
-
-    dynamic "action" {
-        for_each    = var.action
-        content {
-            target_group_arn    = action.value.target_group_arn
-            type                = action.value.type
-
-            dynamic "redirect" {
-                for_each = length(keys(lookup(action.value, "redirect", {}))) == 0 ? [] : [lookup(action.value, "redirect", {})]
-                content {
-                    port        = redirect.value.port
-                    protocol    = redirect.value.protocol
-                    status_code = redirect.value.status_code
-                }
-            }
-        }
-    }
-
-    dynamic "condition" {
-        for_each    = var.condition
-        content {
-            dynamic "path_pattern" {
-                for_each = length(keys(lookup(condition.value, "path_pattern", {}))) == 0 ? [] : [lookup(condition.value, "path_pattern", {})]
-                content {
-                    values  = path_pattern.value.values
-                }
-            }
-        }
-    }
-}
-
